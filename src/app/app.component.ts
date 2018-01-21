800a864d469719176c1a51e05da628d46e190fd9
santhosh-vavilala <santhosh.vavilala@hotmail.com>
2018-01-21 17:51:29 +0530

    Add Sort and Filter method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import  { HttpService } from '../services/http.service'
+import { HttpService } from '../services/http.service'
 
 @Component({
   selector: 'app-root',
@@ -9,22 +9,40 @@ import  { HttpService } from '../services/http.service'
 export class AppComponent {
   title = 'app works!';
   beers = [];
-  constructor(public httpService:HttpService){
+  constructor(public httpService: HttpService) {
     this.title = httpService.msg;
     this.getBeers();
+    this.sort();
   }
 
-  public getBeers()
-  {
-      this.httpService.getBeers().subscribe(data => {console.log(data.json());  this.beers = data.json()});
-  }
-  public sortByName(){
-    
+  public getBeers() {
+    this.httpService.getBeers().subscribe(data => { console.log(data.json()); this.beers = data.json().data });
   }
 
-  public filterByProperty()
-  {
-
+  public sort() {
+    this.beers.sort(function (name1, name2) {
+      if (name1.name < name2.name) {
+        return -1;
+      } else if (name1.name > name2.name) {
+        return 1;
+      } else {
+        return 0;
+      }
+    })
   }
 
+  public orderByName() {
+    this.beers.reverse();
+  }
+
+  public filterByProperty(prop: string, value: string) {
+    console.log('filter method', prop, value)
+    this.beers = this.beers.filter(function (el) {
+      return (el[prop] == value);
+    });
+  }
 }
+
+
+
+
