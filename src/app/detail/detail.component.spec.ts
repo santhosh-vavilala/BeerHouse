81fc006c2f74cb7d67709995d5aad8c61ffb49f0
santhosh-vavilala <santhosh.vavilala@hotmail.com>
2018-01-23 01:45:11 +0530

    Add Sample unit test case for beers service

--- src/app/detail/detail.component.spec.ts
+++ src/app/detail/detail.component.spec.ts
@@ -1,28 +1,57 @@
-/* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import { TestBed, async, inject } from '@angular/core/testing';
+import {
+  HttpModule,
+  Http,
+  Response,
+  ResponseOptions,
+  XHRBackend
+} from '@angular/http';
+import { MockBackend } from '@angular/http/testing';
+import { HttpService } from '../../services/http.service';
 
-import { DetailComponent } from './detail.component';
 
-describe('DetailComponent', () => {
-  let component: DetailComponent;
-  let fixture: ComponentFixture<DetailComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ DetailComponent ]
-    })
-    .compileComponents();
-  }));
+describe('beerService', () => {
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(DetailComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+
+    TestBed.configureTestingModule({
+      imports: [HttpModule],
+      providers: [HttpService,
+        { provide: XHRBackend, useClass: MockBackend },
+      ]
+    });
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('getBeers()', () => {
+
+    it('should return an Observable<Array<beer>>',
+        inject([HttpService, XHRBackend], (beerService, mockBackend) => {
+
+        const mockResponse = {
+          data: [
+            { id: 0, name: 'beer 0' },
+            { id: 1, name: 'beer 1' },
+            { id: 2, name: 'beer 2' },
+            { id: 3, name: 'beer 3' },
+          ]
+        };
+
+        mockBackend.connections.subscribe((connection) => {
+          connection.mockRespond(new Response(new ResponseOptions({
+            body: mockResponse
+          })));
+        });
+
+        beerService.getBeers().subscribe((beers) => {
+          let  res = beers.json().data;
+          console.log(beers.json().data)
+          expect(res.length).toBe(4);
+          expect(res[0].name).toEqual('beer 0');
+          expect(res[1].name).toEqual('beer 1');
+          expect(res[2].name).toEqual('beer 2');
+          expect(res[3].name).toEqual('beer 3');
+        });
+
+    }));
   });
 });
